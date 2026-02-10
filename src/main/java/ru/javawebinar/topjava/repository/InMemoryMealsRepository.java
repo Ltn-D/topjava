487ccd5dedec36607eb29e0d1f4fac4260fa5299
Ltn-D <ltnd@mail.ru>
2026-02-10 12:26:05 +0500

    refactor: InMemoryMealsRepository: add cycle for data init, refactor save()

--- src/main/java/ru/javawebinar/topjava/repository/InMemoryMealsRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/InMemoryMealsRepository.java
@@ -13,50 +13,49 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class InMemoryMealsRepository implements MealsRepository {
     private static final Logger log = getLogger(InMemoryMealsRepository.class);
-    private final Map<Integer, Meal> meals = new ConcurrentHashMap<>();
-    private final AtomicInteger generatorId = new AtomicInteger(0);
 
-    public InMemoryMealsRepository() {
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
-        save(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
+    private final Map<Integer, Meal> mealsMap = new ConcurrentHashMap<>();
+    private final AtomicInteger generatorId = new AtomicInteger(0);
+    private final List<Meal> meals = new ArrayList<>();
+
+    {
+        meals.add(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
+        meals.add(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
+        meals.add(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
+        meals.add(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
+        meals.add(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
+        meals.add(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
+        meals.add(new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
+        meals.forEach(this::save);
     }
 
     @Override
     public List<Meal> findAll() {
-        return new ArrayList<>(meals.values());
+        return new ArrayList<>(mealsMap.values());
     }
 
     @Override
     public Meal findById(int id) {
-        return meals.get(id);
+        return mealsMap.get(id);
     }
 
     @Override
-    public synchronized Meal save(Meal meal) {
+    public Meal save(Meal meal) {
         if (meal.getId() == null) {
             meal.setId(generatorId.incrementAndGet());
-            meals.putIfAbsent(meal.getId(), meal);
+            mealsMap.put(meal.getId(), meal);
             log.debug("create {} is successfully", meal);
+            return meal;
         } else {
-            Meal updatedMeal = meals.getOrDefault(meal.getId(), null);
-            if (updatedMeal == null) {
-                return null;
-            } else {
-                meals.replace(meal.getId(), meal);
-                log.debug("edit {} is successfully", meal);
-            }
+            Meal updatedMeal =  mealsMap.computeIfPresent(meal.getId(), (key, value) -> meal);
+            log.debug("edit {} is successfully", meal);
+            return updatedMeal != null ? meal : null;
         }
-        return meal;
     }
 
     @Override
     public void deleteById(int id) {
-        meals.remove(id);
+        mealsMap.remove(id);
         log.debug("delete meal with id={} is successfully", id);
     }
 }
