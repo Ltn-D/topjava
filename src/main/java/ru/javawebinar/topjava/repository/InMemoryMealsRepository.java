89143fffd4cf3c7eb416aa431b3573573a775476
Ltn-D <ltnd@mail.ru>
2026-02-11 07:55:41 +0500

    refactor: InMemoryMealsRepository

--- src/main/java/ru/javawebinar/topjava/repository/InMemoryMealsRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/InMemoryMealsRepository.java
@@ -18,13 +18,13 @@ public class InMemoryMealsRepository implements MealsRepository {
     private final AtomicInteger generatorId = new AtomicInteger(0);
 
     {
-        Arrays.asList((new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500))
-                , (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000))
-                , (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500))
-                , (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100))
-                , (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000))
-                , (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500))
-                , (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410))).forEach(this::save);
+        Arrays.asList((new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500)),
+                (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000)),
+                (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500)),
+                (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100)),
+                (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000)),
+                (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500)),
+                (new Meal(null, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410))).forEach(this::save);
     }
 
     @Override
